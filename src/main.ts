ce44ce1a31003fed0b9002b0ee74aee2593875dc
spellsaif <this.saif.email@gmail.com>
2023-09-18 05:20:36 +0530

    feat:blockchain class and type

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import {BlockType} from './types'
+import {BlockType, BlockchainType} from './types'
+
+
 
 class Block {
     block: BlockType;
 
-    constructor(previousHash:string, transactions: string[]) {
+    constructor(previousHash:string, nonce: number) {
         const timestamp = this.getUnixTime();
-        this.block = {previousHash, transactions, timestamp, nonce: Math.ceil(Math.random()* 1000)}
+        this.block = {previousHash, timestamp, transactions:[], nonce}
     }
 
     getUnixTime() {
@@ -18,11 +20,27 @@ class Block {
 
 }
 
-function main() {
-    const b = new Block("",[])
+class Blockchain {
+    blockchain:BlockchainType = {
+        chain: [],
+        transactionPool: []
+    }
+    constructor() {
+        const b = new Block("Genesis", 0).block;
+        this.blockchain.chain.push(b)
+    }
 
-    
-    b.print()
+    createBlock(previousHash: string, nonce: number) {
+        const newBlock = new Block(previousHash, nonce).block;
+        this.blockchain.chain.push(newBlock)
+        return newBlock;
+    }
+}
+
+function main() {
+    const bc = new Blockchain();
+    bc.createBlock("hash 1", 1);
+    console.table(bc.blockchain.chain)
 }
 
 main();
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,13 @@
+
+
 export type BlockType = {
     previousHash: string;
     transactions: string[];
     timestamp: number;
     nonce: number;
+}
+
+export type BlockchainType = {
+    transactionPool?: string[];
+    chain: BlockType[];
 }
