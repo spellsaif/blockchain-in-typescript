4a434e32bae1cd71d3e35ac0630e6fe736df9a1d
spellsaif <this.saif.email@gmail.com>
2023-09-18 04:45:13 +0530

    feat: block

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,30 @@
-console.log("Hello World!")
+import {BlockType} from './types'
+
+class Block {
+    block: BlockType;
+
+    constructor(previousHash:string, transactions: string[]) {
+        const timestamp = this.getUnixTime();
+        this.block = {previousHash, transactions, timestamp, nonce: Math.ceil(Math.random()* 1000)}
+    }
+
+    getUnixTime() {
+        return Math.ceil(new Date().getTime()/1000)
+    }
+
+    print() {
+        console.table(this.block)
+    }
+
+}
+
+function main() {
+    const b = new Block("",[])
+
+    
+    b.print()
+}
+
+main();
+
+
---
+++ src/types.ts
@@ -0,0 +1,6 @@
+export type BlockType = {
+    previousHash: string;
+    transactions: string[];
+    timestamp: number;
+    nonce: number;
+}
